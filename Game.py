59b449869713384e44901703fd8cd1f12516fc89
Juodeikis <juodeikis2006@gmail.com>
2026-04-13 23:06:17 +0300

    added tile collisions

--- Game.py
+++ Game.py
@@ -10,26 +10,23 @@ class Game:
         pygame.init()
         
         pygame.display.set_caption("square cubed")
-        self.screen = pygame.display.set_mode((640, 480))
-        #self.screen = pygame.Surface((6400, 4800))
+        self.display = pygame.display.set_mode((640, 480)) 
+        self.screen = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
         self.movement = [False, False]
-        self.collission = pygame.Rect(50, 50, 30, 50)
 
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone')
-        }
+        } #loads assets
 
         
-        self.player = PhysicsEntity(self, 'player', (50, 50), (48, 48))
+        self.player = PhysicsEntity(self, 'player', (50, 50), (15, 15))
         
         self.tilemap = Tilemap(self, 16)
-        
-        print(self.tilemap.tiles_around(self.player.pos))
         
     def run(self):
         while True:
@@ -46,6 +43,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
@@ -53,16 +52,10 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
-            img_r = pygame.Rect(*self.player.pos, *self.player.size)
-
-            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.screen)
-            if img_r.colliderect(self.collission):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collission)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 200), self.collission)
             
-            #self.display.blit(pygame.transform.scale(self.screen, self.display.get_size()), (0, 0))
+            self.display.blit(pygame.transform.scale(self.screen, self.display.get_size()), (0, 0))
             
             pygame.display.update()
             
--- scripts/entities.py
+++ scripts/entities.py
@@ -6,15 +6,49 @@ class PhysicsEntity:
         self.type = e_type
         self.pos = list(pos)
         self.size = list(size)
-        self.velocity = [0, 2]
+        self.velocity = [0, 0]
 
-    def update(self, movement=(0,0)):
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def update(self, tilemap, movement=(0,0)):
+        self.collisions = {'up': False, 'down': False, 'left': False, 'right': False}
+
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity [1])
-        
-        self.velocity[1] = min(1, self.velocity[1]+0.1)
-        
+
         self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
+
         self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+        if self.collisions['right'] or self.collisions['left']:
+            self.velocity[0] = 0
+
+        self.velocity[1] = min(5, self.velocity[1]+0.1)
+
+        if self.collisions['down'] or self.collisions['up']:
+            self.velocity[1] = 0
 
     def render(self, surface):
         pygame.draw.rect(surface, (100, 200, 0), pygame.Rect(*self.pos, *self.size))
